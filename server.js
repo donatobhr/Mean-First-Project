2b8ea96380f218fcf93b0be7db7394b8aeee0a5e
Donato Bencosme <donatobhr@gmail.com>
2018-03-22 23:42:04 -0400

    chap_11 completed, book finished

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
-// development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// test
 const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
 const configurePassport = require('./config/passport');
